9ae71472ca824fdc9ef34ccb663577551097b97a
Roland Sharp <rolandnsharp@gmail.com>
2014-08-10 18:14:08 +1000

    add heroku remote

--- index.js
+++ index.js
@@ -12,34 +12,44 @@ var nearestData = {
     distance: 99999999999999999999
 }
 
+function latLngToPostcode(lat, lng, callback) {
+    fs.readFile('./postcodeLatLng.csv', function(err, data) {
+        if (err) throw err;
+        var rows = data.toString().split("\n");
+        for (i in rows) {
 
-fs.readFile('./postcodeLatLng.csv', function(err, data) {
-    if (err) throw err;
-    var rows = data.toString().split("\n");
-    for (i in rows) {
+            var cells = rows[i].split(",");
+            var postcode = cells[0];
+            var lat = cells[5] && parseFloat(cells[5].replace("\"", ""));
+            var lng = cells[6] && parseFloat(cells[6].replace("\"", ""));
 
-        var cells = rows[i].split(",");
-        var postcode = cells[0];
-        var lat = cells[5] && parseFloat(cells[5].replace("\"", ""));
-        var lng = cells[6] && parseFloat(cells[6].replace("\"", ""));
-
-        vincenty.distVincenty(clientLat, clientLng, lat, lng, function(distance) {
-            if (nearestData.distance > distance) {
-                // console.log(distance);
-                nearestData = {
-                    postcode: postcode,
-                    distance: distance
-                };
-            }
-        });
+            vincenty.distVincenty(clientLat, clientLng, lat, lng, function(distance) {
+                if (nearestData.distance > distance) {
+                    // console.log(distance);
+                    nearestData = {
+                        postcode: postcode,
+                        distance: distance
+                    };
+                }
+            });
 
 
-    }
-    console.log(nearestData);
-});
+        }
+        callback(nearestData);
+    });
+}
 
 app.get('/', function(req, res) {
-    res.send('hello  world');
+    res.send('hello  world' + req.query.lat + req.query.lng);
+
+    latLngToPostcode(req.query.lat, req.query.lng, function(postcode) {
+        console.log(postcode);
+    });
+
+
+
+
+
 });
 
 var port = Number(process.env.PORT || 5000);
