6db88776b5391960cf4b573d0283e5db5e64900e
Roland Sharp <rolandnsharp@gmail.com>
2014-08-13 23:23:16 +1000

    updated legislator contacts parser to include both senators and representatives, inculde member_iss and null member_ids

--- parseLegislators.js
+++ parseLegislators.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
-var getRepresentatives = require('./getSenators');
+var getSenators = require('./getSenators');
+var getRepresentatives = require('./getRepresentatives');
 
 var newAusMpContacts = '';
 
@@ -14,7 +15,7 @@ fs.readFile('./aus_mp_contact_details.csv', function(err, data) {
 
         var cell = cells[2];
         var foundMatch = false;
-        getRepresentatives.forEach(function(rep) {
+        getSenators.forEach(function(rep) {
             member_id = rep.member_id,
             first_name = rep.first_name,
             last_name = rep.last_name
@@ -33,6 +34,25 @@ fs.readFile('./aus_mp_contact_details.csv', function(err, data) {
         if (!foundMatch && cells[0] !== 'representatives') {
             newAusMpContacts += rows[i] + ',' + null + '\n';
         }
+        getRepresentatives.forEach(function(rep) {
+            member_id = rep.member_id,
+            first_name = rep.first_name,
+            last_name = rep.last_name
+
+            // console.log(cell);
+            if (typeof cell !== 'undefined' && (cell.indexOf(first_name) !== -1) && (cell.indexOf(last_name) !== -1)) {
+
+                console.log(member_id, first_name, last_name);
+                console.log(cells[0], cells[1], cells[2]);
+                console.log('*********************************************');
+                newAusMpContacts += rows[i] + ',' + member_id + '\n';
+                foundMatch = true;
+
+            }
+        });
+        if (!foundMatch && cells[0] !== 'senate') {
+            newAusMpContacts += rows[i] + ',' + null + '\n';
+        }
     }
-    fs.writeFileSync('./newSenatorsWithMemberId.csv', newAusMpContacts);
+    fs.writeFileSync('./legislator_contacts_with_member_id.csv', newAusMpContacts);
 });
